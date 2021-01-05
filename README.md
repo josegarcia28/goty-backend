c852e2cf086e53c5cb1d51ea573355c5263c3cc8
Jose Garcia <josegarcia28@gmail.com>
2021-01-05 20:40:33 +0100

    version final de proyecto

---
+++ README.md
@@ -0,0 +1,2 @@
+# grafica-backend
+API para gestion de botos de pagina de graficas para votacion de juegos
---
+++ functions/src/index.ts
@@ -0,0 +1,81 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+import * as express from 'express';
+import * as cors from 'cors';
+
+
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://firestore-grafica-ef706.firebaseio.com"
+  });
+
+  const db = admin.firestore();
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld = functions.https.onRequest((request, response) => {
+ response.json({
+     mensaje: "Hola desde funciones de firebase!!"
+    });
+});
+export const getGoty = functions.https.onRequest(async(request, response) => {
+ 
+ // const nombre= request.query.nombre || 'Sin nombre';
+
+//  response.json({
+//      nombre
+//  })
+
+const gotyRef = db.collection('goty');
+const docsSnap = await gotyRef.get();
+const juegos = docsSnap.docs.map( doc => doc.data());
+
+response.json( juegos )
+
+});
+
+// configuracion express ///////////////////////////////
+const app = express();
+app.use( cors({ origin: true}) );
+
+app.get('/goty', async(req, res) => {
+
+  const gotyRef = db.collection('goty');
+  const docsSnap = await gotyRef.get();
+  const juegos = docsSnap.docs.map( doc => doc.data());
+  
+  res.json( juegos )
+
+});
+
+app.post('/goty/:id', async(req, res) => {
+
+  const id = req.params.id;
+  const gameRef = db.collection('goty').doc( id );
+  const gameSnap = await gameRef.get();
+  
+  if ( !gameSnap.exists ) {
+    res.status(404).json({
+      ok: false,
+      mensaje: 'No existe un juego con ese ID ' + id
+    });
+  } else {
+    const antes = gameSnap.data() || { votos: 0 };
+    await gameRef.update({
+      votos: antes.votos + 1
+    });
+
+    res.json({
+      ok: true,
+      mensaje: `Gracias por tu voto a ${ antes.name }`
+    });
+  }
+
+});
+
+export const api = functions.https.onRequest( app );
+////////////////////////////////////////////////
